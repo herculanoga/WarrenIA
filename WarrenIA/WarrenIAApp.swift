99611a2a6c9fcf5412c0f7eb19eef4a845ea13c2
Gabriel Herculano <herculano1977@outlook.com>
2026-04-22 14:19:59 +0200

    Initial Commit

---
+++ WarrenIA/WarrenIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarrenIAApp.swift
+//  WarrenIA
+//
+//  Created by Herculano on 22/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WarrenIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
